383ef92a4765abbdb9e73f2b3b8ce8e3511a507d
nix <609610903@qq.com>
2025-03-03 16:29:39 +0800

    message : 更改摄像头水平镜像 modified: components/modules/camera/who_camera.c Lines   : 89

--- components/modules/camera/who_camera.c
+++ components/modules/camera/who_camera.c
@@ -86,6 +86,7 @@ void register_camera(const pixformat_t pixel_fromat,
 
     sensor_t *s = esp_camera_sensor_get();
     s->set_vflip(s, 0); //flip it back
+    s->set_hmirror(s, 1);
     
     //initial sensors are flipped vertically and colors are a bit saturated
     if (s->id.PID == OV3660_PID)
